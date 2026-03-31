9695c739a4ef06b5fdff056b0d9f127eb9467264
Ruturaj Amit Rajwade <ruturaj.rajwade@gmail.com>
2026-03-31 13:28:04 +0530

    Fix. hover animation on background

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <>
       <div className="relative min-h-screen ">
         {/* background */}
-        <div className="absolute inset-0 -z-10  bg-black">
+        <div className="absolute inset-0 z-0  bg-black">
           <Antigravity particleShape="box" particleSize={1.8} color="#5ca3ff" />
         </div>
         {/* Content */}
